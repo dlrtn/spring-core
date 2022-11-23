55901b287b32b5e641cf701afcfda6b9451a2d54
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-11-24 00:53:07 +0900

    의존관계 자동 주입

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,6 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @Component
